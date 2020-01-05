fc1c286a46c2775115e715cb3495f9636e8511fd
Ankit Pati <contact@ankitpati.in>
2020-01-05 12:51:01 +0530

    security lockdown, ref. electronjs.org/docs/tutorial/security

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const { app, session, BrowserWindow } = require('electron');
 
 let win;
 
 function createWindow() {
-    let win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
+            enableRemoteModule: false,
             nodeIntegration: false,
             nodeIntegrationInWorker: false,
             contextIsolation: true
@@ -19,6 +20,23 @@ function createWindow() {
         win = null;
     });
 }
+
+app.on('web-contents-created', (event, contents) => {
+    /* <webview>s disallowed */
+    contents.on('will-attach-webview', (event, webPreferences, params) => {
+        event.preventDefault();
+    });
+
+    /* navigation disallowed */
+    contents.on('will-navigate', (event, navigationUrl) => {
+        event.preventDefault();
+    });
+
+    /* new windows disallowed */
+    contents.on('new-window', (event, navigationUrl) => {
+        event.preventDefault();
+    });
+});
 
 app.on('ready', createWindow);
 
